06334fdac2d5927eaa430aa9d1d32c9441a0a2ed
Andy-Aseye <andyaseye@gmail.com>
2023-07-05 13:17:54 +0000

    modifications made to pdf design

--- platform/viewer/src/lib/PdfMaker.js
+++ platform/viewer/src/lib/PdfMaker.js
@@ -50,55 +50,100 @@ function createSimilarScansSection(
                 },
                 {
                   image: scanImg,
-                  width: 100,
-                  height: 80,
-                  margin: [0, -147, 0, 20],
+                  width: 98,
+                  height: 70,
+                  margin: [1, -119, 0, 20],
                 },
                 {
-                  margin: [10, -9, 0, 10],
+                  margin: [1, -13, 0, 20],
                   stack: [
                     {
+                      columnGap: 0,
                       columns: [
-                        { text: 'Similarity:', bold: true },
-                        { text: similarity, margin: [-18, 0, 0, 0] },
-                      ],
-                    },
-                    {
-                      columns: [
-                        { text: 'Dataset:', bold: true },
-                        { text: dataset, margin: [-18, 0, 0, 0] },
-                      ],
-                    },
-                    {
-                      columns: [
+                        { text: '0' + dataset },
                         {
-                          text: 'Dataset Id:',
-                          bold: true,
-                          margin: [-1, 0, 0, 0],
-                        },
-                        { text: datasetId, margin: [-18, 0, 0, 0] },
-                      ],
-                    },
-                    {
-                      columns: [
-                        { text: 'Malignant:', bold: true },
-                        {
-                          text: malignant ? 'Yes' : 'No',
-                          color: malignant ? 'red' : 'blue',
-                          margin: [-18, 0, 0, 0],
+                          margin: [-30, 0, 0, 0],
+                          stack: [
+                            {
+                              columns: [
+                                {
+                                  text: 'Similarity:',
+                                  bold: true,
+                                  fontSize: 8,
+                                },
+                                {
+                                  text: similarity,
+                                  fontSize: 8,
+                                  margin: [0, 0, 0, 0],
+                                },
+                              ],
+                            },
+                            {
+                              columns: [
+                                {
+                                  text: 'Dataset:',
+                                  fontSize: 8,
+                                  bold: true,
+                                },
+                                {
+                                  text: dataset,
+                                  fontSize: 8,
+                                  margin: [0, 0, 0, 0],
+                                },
+                              ],
+                            },
+                            {
+                              columnGap: 3,
+                              columns: [
+                                {
+                                  text: 'Dataset Id:',
+                                  fontSize: 8,
+                                  bold: true,
+                                  margin: [-1, 0, 0, 0],
+                                },
+                                {
+                                  text: datasetId,
+                                  fontSize: 8,
+                                  margin: [0, 0, 0, 0],
+                                },
+                              ],
+                            },
+                            {
+                              columns: [
+                                {
+                                  text: 'Malignant:',
+                                  bold: true,
+                                  fontSize: 8,
+                                },
+                                {
+                                  text: malignant ? 'Yes' : 'No',
+                                  color: malignant ? 'red' : 'blue',
+                                  fontSize: 8,
+                                  margin: [2, 0, 0, 0],
+                                },
+                              ],
+                            },
+                          ],
                         },
                       ],
                     },
                   ],
                 },
+                {
+                  stack: [
+                    {
+                      image: scanLargeImg,
+                      width: 130,
+                      height: 120,
+                      margin: [-10, 5, 0, 0],
+                    },
+                  ],
+                  margin: [20, 0, 0, 0],
+                },
               ],
             },
           ],
         ],
-      },
-      {
-        stack: [{ image: scanLargeImg, width: 290, height: 285 }],
-        margin: [40, 0, 0, 0],
       },
     ],
   };
@@ -118,122 +163,241 @@ function createSimilarScansSection2(
     alignment: 'justify',
     columns: [
       {
-        width: 'auto',
-        style: 'smallImg',
-        stack: [
+        columns: [
           {
-            bold: true,
-            text: '0' + dataset,
-            fontSize: 18, // bigger, bolder font for jumbotron header
-            margin: [0, 20, 0, 0],
-          },
-          {
-            margin: [0, 10, 0, 10],
+            width: 'auto',
+            style: 'smallImg',
             stack: [
               {
-                image: originalQueryImg,
-                width: 100,
-                height: 80,
-              },
-              {
-                margin: [0, 5, 0, 10],
-                text: 'Original Query',
+                stack: [
+                  {
+                    canvas: [
+                      {
+                        type: 'rect',
+                        x: 0,
+                        y: 0,
+                        w: 100,
+                        h: 122,
+                        lineWidth: 1,
+                        lineColor: malignant ? 'red' : 'blue',
+                      },
+                    ],
+                  },
+                  {
+                    image: originalQueryImg,
+                    width: 98,
+                    height: 72,
+                    margin: [1, -121, 0, 20],
+                  },
+                  {
+                    margin: [10, -13, 0, 20],
+                    // padding: [20, 10, 20, 10],
+                    stack: [
+                      {
+                        columnGap: 0,
+                        columns: [
+                          { text: '04', bold: true },
+                          {
+                            margin: [-30, 0, 0, 0],
+                            stack: [
+                              {
+                                columns: [
+                                  {
+                                    text: 'Similarity:',
+                                    bold: true,
+                                    fontSize: 8,
+                                  },
+                                  {
+                                    text: similarity,
+                                    fontSize: 8,
+                                    margin: [0, 0, 0, 0],
+                                  },
+                                ],
+                              },
+                              {
+                                columns: [
+                                  {
+                                    text: 'Dataset:',
+                                    fontSize: 8,
+                                    bold: true,
+                                  },
+                                  {
+                                    text: dataset,
+                                    fontSize: 8,
+                                    margin: [0, 0, 0, 0],
+                                  },
+                                ],
+                              },
+                              {
+                                columns: [
+                                  {
+                                    text: 'Dataset Id:',
+                                    fontSize: 8,
+                                    bold: true,
+                                    margin: [-1, 0, 0, 0],
+                                  },
+                                  {
+                                    text: datasetId,
+                                    fontSize: 8,
+                                    margin: [3, 0, 0, 0],
+                                  },
+                                ],
+                              },
+                              {
+                                columns: [
+                                  {
+                                    text: 'Malignant:',
+                                    bold: true,
+                                    fontSize: 8,
+                                  },
+                                  {
+                                    text: malignant ? 'Yes' : 'No',
+                                    color: malignant ? 'red' : 'blue',
+                                    fontSize: 8,
+                                    margin: [2, 0, 0, 0],
+                                  },
+                                ],
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
               },
             ],
           },
-          [
-            {
-              stack: [
-                {
-                  canvas: [
-                    {
-                      type: 'rect',
-                      x: 0,
-                      y: 0,
-                      w: 100,
-                      h: 140,
-                      lineWidth: 1,
-                      lineColor: malignant ? 'red' : 'blue',
-                    },
-                  ],
-                },
-                {
-                  image: scanImg,
-                  width: 100,
-                  height: 80,
-                  margin: [0, -140, 0, 20],
-                },
-                {
-                  margin: [10, -13, 0, 20],
-                  // padding: [20, 10, 20, 10],
-                  stack: [
-                    // columns: [ ],
-                    {
-                      columns: [
-                        {
-                          text: 'Similarity:',
-                          bold: true,
-                        },
-                        {
-                          text: similarity,
-                          margin: [-18, 0, 0, 0],
-                        },
-                      ],
-                    },
-                    {
-                      columns: [
-                        {
-                          text: 'Dataset:',
-                          bold: true,
-                        },
-                        {
-                          text: dataset,
-                          margin: [-18, 0, 0, 0],
-                        },
-                      ],
-                    },
-                    {
-                      columns: [
-                        {
-                          text: 'Dataset Id:',
-                          bold: true,
-                          margin: [-1, 0, 0, 0],
-                        },
-                        {
-                          text: datasetId,
-                          margin: [-18, 0, 0, 0],
-                        },
-                      ],
-                    },
-                    {
-                      columns: [
-                        {
-                          text: 'Malignant:',
-                          bold: true,
-                        },
-                        {
-                          text: malignant ? 'Yes' : 'No',
-                          color: malignant ? 'red' : 'blue',
-                          margin: [-18, 0, 0, 0],
-                        },
-                      ],
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
+          {
+            stack: [
+              {
+                image: scanLargeImg,
+                width: 130,
+                height: 120,
+              },
+            ],
+            margin: [5, 0, 5, 0],
+          },
         ],
       },
       {
-        stack: [
+        columns: [
           {
-            image: scanLargeImg,
-            width: 290,
-            height: 285,
+            width: 'auto',
+            style: 'smallImg',
+            stack: [
+              {
+                stack: [
+                  {
+                    canvas: [
+                      {
+                        type: 'rect',
+                        x: 0,
+                        y: 0,
+                        w: 100,
+                        h: 120,
+                        lineWidth: 1,
+                        lineColor: malignant ? 'red' : 'blue',
+                      },
+                    ],
+                  },
+                  {
+                    image: scanImg,
+                    width: 98,
+                    height: 72,
+                    margin: [1, -119, 0, 20],
+                  },
+                  {
+                    margin: [10, -13, 0, 20],
+                    // padding: [20, 10, 20, 10],
+                    stack: [
+                      {
+                        columnGap: 0,
+                        columns: [
+                          { text: '0' + dataset, bold: true },
+                          {
+                            margin: [-30, 0, 0, 0],
+                            stack: [
+                              {
+                                columns: [
+                                  {
+                                    text: 'Similarity:',
+                                    bold: true,
+                                    fontSize: 8,
+                                  },
+                                  {
+                                    text: similarity,
+                                    fontSize: 8,
+                                    margin: [0, 0, 0, 0],
+                                  },
+                                ],
+                              },
+                              {
+                                columns: [
+                                  {
+                                    text: 'Dataset:',
+                                    fontSize: 8,
+                                    bold: true,
+                                  },
+                                  {
+                                    text: dataset,
+                                    fontSize: 8,
+                                    margin: [0, 0, 0, 0],
+                                  },
+                                ],
+                              },
+                              {
+                                columns: [
+                                  {
+                                    text: 'Dataset Id:',
+                                    fontSize: 8,
+                                    bold: true,
+                                    margin: [-1, 0, 0, 0],
+                                  },
+                                  {
+                                    text: datasetId,
+                                    fontSize: 8,
+                                    margin: [3, 0, 0, 0],
+                                  },
+                                ],
+                              },
+                              {
+                                columns: [
+                                  {
+                                    text: 'Malignant:',
+                                    bold: true,
+                                    fontSize: 8,
+                                  },
+                                  {
+                                    text: malignant ? 'Yes' : 'No',
+                                    color: malignant ? 'red' : 'blue',
+                                    fontSize: 8,
+                                    margin: [2, 0, 0, 0],
+                                  },
+                                ],
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                      // columns: [ ],
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            stack: [
+              {
+                image: scanLargeImg,
+                width: 130,
+                height: 120,
+              },
+            ],
+            margin: [10, 0, 0, 0],
           },
         ],
-        margin: [40, 0, 0, 0],
       },
     ],
   };
@@ -253,7 +417,7 @@ function createReportSummaryTable(
           {
             text: 'RadCard Report Summary',
             style: 'jumbotronHeader',
-            colSpan: 2,
+            // colSpan: 2,
           },
           {},
         ],
@@ -406,19 +570,51 @@ const PdfMaker = (SimilarScans, ohif_image, chart, morphologyBase64) => {
       },
       header2: { fontSize: 16, bold: true, margin: [0, 10, 0, 10] },
       header3: { fontSize: 14, bold: true, margin: [20, 140, 0, 5] },
+      headercol: {
+        columnGap: 0,
+        width: 200,
+        margin: [0, -20, 0, -19],
+      },
       smallImg: { fontSize: 10, margin: [0, 0, 0, 5] },
       imagescol: { columnGap: 20, margin: [10, 50, 30, 10] },
       imagescol2: {
-        columnGap: 20,
-        margin: [10, 10, 30, 30],
+        columnGap: 60,
+        margin: [10, 7, 30, 7],
+      },
+      imagescol3: {
+        columnGap: 40,
+        margin: [0, 7, 30, 7],
       },
       jumbotron: {
         fillColor: '#F9FAFA', // jumbotron background color
         padding: [10, 10, 10, 10], // add padding for the jumbotron
         border: 'grey',
       },
+      jumbotronBlue: {
+        fillColor: '#243D4E', // jumbotron background color
+        padding: [10, 0, 10, 10], // add padding for the jumbotron
+        border: 'grey',
+        color: 'white',
+      },
+      jumbotronLBlue: {
+        fillColor: '#A2B1BC', // jumbotron background color
+        padding: [10, -10, 10, 0], // add padding for the jumbotron
+        border: 'grey',
+        color: 'white',
+      },
+      jumbotronLBlue2: {
+        fillColor: '#A2B1BC', // jumbotron background color
+        padding: [10, 0, 10, 10], // add padding for the jumbotron
+        border: 'grey',
+        color: 'white',
+      },
+      jumbotronDark: {
+        fillColor: '#EFF0F5', // jumbotron background color
+        padding: [10, 10, 10, 10], // add padding for the jumbotron
+        border: 'grey',
+      },
       jumbotronHeader: {
-        fontSize: 17, // bigger, bolder font for jumbotron header
+        fontSize: 12, // bigger, bolder font for jumbotron header
         bold: true, // make the header bold
         margin: [12, 10, 10, 10], // add some margin to create space under the header
       },
